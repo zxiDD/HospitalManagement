133cf0d68fe94889ca380c69edc3f83d602a1a50
Zaid <kzaid2144@gmail.com>
2026-04-24 15:04:35 +0530

    initialized backend

---
+++ backend/HospitalManagement/src/test/java/com/cg/hsm/HospitalManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cg.hsm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HospitalManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
