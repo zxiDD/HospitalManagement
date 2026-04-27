244e5ef87cc1d8122118a2c5523acdb6b4016b28
Zaid <kzaid2144@gmail.com>
2026-04-27 10:05:08 +0530

    created entity classes

---
+++ backend/HospitalManagement/src/main/java/com/cg/entity/Procedures.java
@@ -0,0 +1,57 @@
+package com.cg.entity;
+
+import java.math.BigDecimal;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "Procedures")
+public class Procedures {
+	 @Id
+	    @Column(name = "Code")
+	    private Integer code;
+
+	    @Column(name = "Name", nullable = false, length = 255)
+	    private String name;
+
+	    @Column(name = "Cost", nullable = false, precision = 10, scale = 2)
+	    private BigDecimal cost;
+
+	    // Constructors
+	    public Procedures() {
+	    }
+
+	    public Procedures(Integer code, String name, BigDecimal cost) {
+	        this.code = code;
+	        this.name = name;
+	        this.cost = cost;
+	    }
+
+	    // Getters and Setters
+	    public Integer getCode() {
+	        return code;
+	    }
+
+	    public void setCode(Integer code) {
+	        this.code = code;
+	    }
+
+	    public String getName() {
+	        return name;
+	    }
+
+	    public void setName(String name) {
+	        this.name = name;
+	    }
+
+	    public BigDecimal getCost() {
+	        return cost;
+	    }
+
+	    public void setCost(BigDecimal cost) {
+	        this.cost = cost;
+	    }
+}
