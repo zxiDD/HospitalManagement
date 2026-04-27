860750d1926b6c7bd91103928c1ed675ee3b5a16
Aradhya24tyagi <tyagiaradhya54@gmail.com>
2026-04-27 10:34:10 +0530

    ServicePackage created

--- backend/HospitalManagement/src/main/java/com/cg/entity/Department.java
+++ backend/HospitalManagement/src/main/java/com/cg/entity/Department.java
@@ -18,5 +18,29 @@ public class Department {
     @JoinColumn(name = "head", nullable = false)
     private Physician head;
 
+	public Integer getDepartmentId() {
+		return departmentId;
+	}
+
+	public void setDepartmentId(Integer departmentId) {
+		this.departmentId = departmentId;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Physician getHead() {
+		return head;
+	}
+
+	public void setHead(Physician head) {
+		this.head = head;
+	}
+
 }
 
