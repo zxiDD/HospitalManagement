7c1274c8eb18736b9c155fddc8f528178a63097f
Aradhya24tyagi <tyagiaradhya54@gmail.com>
2026-04-27 14:12:14 +0530

    dtos created

---
+++ backend/HospitalManagement/src/main/java/com/cg/dto/DepartmentDTO.java
@@ -0,0 +1,52 @@
+package com.cg.dto;
+
+public class DepartmentDTO {
+
+    private Integer departmentId;
+    private String name;
+    private Integer headId;     // physician employeeId
+    private String headName;    // optional (useful for UI)
+
+    // Constructors
+    public DepartmentDTO() {}
+
+    public DepartmentDTO(Integer departmentId, String name, Integer headId, String headName) {
+        this.departmentId = departmentId;
+        this.name = name;
+        this.headId = headId;
+        this.headName = headName;
+    }
+
+    // Getters & Setters
+    public Integer getDepartmentId() {
+        return departmentId;
+    }
+
+    public void setDepartmentId(Integer departmentId) {
+        this.departmentId = departmentId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getHeadId() {
+        return headId;
+    }
+
+    public void setHeadId(Integer headId) {
+        this.headId = headId;
+    }
+
+    public String getHeadName() {
+        return headName;
+    }
+
+    public void setHeadName(String headName) {
+        this.headName = headName;
+    }
+}
