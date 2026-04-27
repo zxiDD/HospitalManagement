fd94e26f80ccf4b821fd0b9e1cd2ec15289ed69b
Aashi Chauhan <aashichauhan6020@gmail.com>
2026-04-27 13:01:40 +0530

    done changes

--- backend/HospitalManagement/src/main/java/com/cg/dto/OnCallDTO.java
+++ backend/HospitalManagement/src/main/java/com/cg/dto/OnCallDTO.java
@@ -4,9 +4,9 @@ import java.time.LocalDateTime;
 
 public class OnCallDTO {
 
-    private int nurseId;
-    private int blockFloor;
-    private int blockCode;
+    private Integer nurseId;
+    private Integer blockFloor;
+    private Integer blockCode;
 
     private LocalDateTime onCallStart;
     private LocalDateTime onCallEnd;
@@ -14,53 +14,55 @@ public class OnCallDTO {
     public OnCallDTO() {
     }
 
-    public OnCallDTO(int nurseId, int blockFloor, int blockCode,
-                     LocalDateTime onCallStart, LocalDateTime onCallEnd) {
-        this.nurseId = nurseId;
-        this.blockFloor = blockFloor;
-        this.blockCode = blockCode;
-        this.onCallStart = onCallStart;
-        this.onCallEnd = onCallEnd;
-    }
+	public OnCallDTO(Integer nurseId, Integer blockFloor, Integer blockCode, LocalDateTime onCallStart,
+			LocalDateTime onCallEnd) {
+		super();
+		this.nurseId = nurseId;
+		this.blockFloor = blockFloor;
+		this.blockCode = blockCode;
+		this.onCallStart = onCallStart;
+		this.onCallEnd = onCallEnd;
+	}
+
+	public Integer getNurseId() {
+		return nurseId;
+	}
+
+	public void setNurseId(Integer nurseId) {
+		this.nurseId = nurseId;
+	}
+
+	public Integer getBlockFloor() {
+		return blockFloor;
+	}
+
+	public void setBlockFloor(Integer blockFloor) {
+		this.blockFloor = blockFloor;
+	}
+
+	public Integer getBlockCode() {
+		return blockCode;
+	}
+
+	public void setBlockCode(Integer blockCode) {
+		this.blockCode = blockCode;
+	}
+
+	public LocalDateTime getOnCallStart() {
+		return onCallStart;
+	}
+
+	public void setOnCallStart(LocalDateTime onCallStart) {
+		this.onCallStart = onCallStart;
+	}
+
+	public LocalDateTime getOnCallEnd() {
+		return onCallEnd;
+	}
+
+	public void setOnCallEnd(LocalDateTime onCallEnd) {
+		this.onCallEnd = onCallEnd;
+	}
 
 
-    public int getNurseId() {
-        return nurseId;
-    }
-
-    public void setNurseId(int nurseId) {
-        this.nurseId = nurseId;
-    }
-
-    public int getBlockFloor() {
-        return blockFloor;
-    }
-
-    public void setBlockFloor(int blockFloor) {
-        this.blockFloor = blockFloor;
-    }
-
-    public int getBlockCode() {
-        return blockCode;
-    }
-
-    public void setBlockCode(int blockCode) {
-        this.blockCode = blockCode;
-    }
-
-    public LocalDateTime getOnCallStart() {
-        return onCallStart;
-    }
-
-    public void setOnCallStart(LocalDateTime onCallStart) {
-        this.onCallStart = onCallStart;
-    }
-
-    public LocalDateTime getOnCallEnd() {
-        return onCallEnd;
-    }
-
-    public void setOnCallEnd(LocalDateTime onCallEnd) {
-        this.onCallEnd = onCallEnd;
-    }
 }
