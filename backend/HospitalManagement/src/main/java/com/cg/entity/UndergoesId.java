bc03148a35b3a20fbb41fae104f7b9e7f9d101b0
Zaid <kzaid2144@gmail.com>
2026-04-27 10:29:04 +0530

    created entity for undergoes

---
+++ backend/HospitalManagement/src/main/java/com/cg/entity/UndergoesId.java
@@ -0,0 +1,66 @@
+package com.cg.entity;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Embeddable;
+
+@Embeddable
+public class UndergoesId implements Serializable {
+	@Column(name = "patient")
+	private Long patient;
+
+	@Column(name = "procedures")
+	private Integer procedures;
+
+	@Column(name = "stay")
+	private Integer stay;
+
+	@Column(name = "date_undergoes")
+	private LocalDateTime dateUndergoes;
+
+	public UndergoesId() {
+
+	}
+
+	public UndergoesId(Long patient, Integer procedures, Integer stay, LocalDateTime dateUndergoes) {
+		super();
+		this.patient = patient;
+		this.procedures = procedures;
+		this.stay = stay;
+		this.dateUndergoes = dateUndergoes;
+	}
+
+	public Long getPatient() {
+		return patient;
+	}
+
+	public void setPatient(Long patient) {
+		this.patient = patient;
+	}
+
+	public Integer getProcedures() {
+		return procedures;
+	}
+
+	public void setProcedures(Integer procedures) {
+		this.procedures = procedures;
+	}
+
+	public Integer getStay() {
+		return stay;
+	}
+
+	public void setStay(Integer stay) {
+		this.stay = stay;
+	}
+
+	public LocalDateTime getDateUndergoes() {
+		return dateUndergoes;
+	}
+
+	public void setDateUndergoes(LocalDateTime dateUndergoes) {
+		this.dateUndergoes = dateUndergoes;
+	}
+}
