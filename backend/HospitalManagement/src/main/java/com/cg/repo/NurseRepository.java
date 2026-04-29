7b05ddbcebd31948ff2074ca6523f721ae3f196c
Zaid <kzaid2144@gmail.com>
2026-04-29 13:40:44 +0530

    fixed missing Repository annotation

--- backend/HospitalManagement/src/main/java/com/cg/repo/NurseRepository.java
+++ backend/HospitalManagement/src/main/java/com/cg/repo/NurseRepository.java
@@ -7,7 +7,9 @@ import java.time.LocalDateTime;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface NurseRepository extends JpaRepository<Nurse, Integer> {
 	@Query("""
 			    SELECT COUNT(oc) > 0
