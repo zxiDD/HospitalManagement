8e24fc2afe6d8235002e98e12d112fd439ff4c8c
Aashi Chauhan <aashichauhan6020@gmail.com>
2026-04-29 14:43:21 +0530

    soft delete done...

--- backend/HospitalManagement/src/main/java/com/cg/controller/AppointmentController.java
+++ backend/HospitalManagement/src/main/java/com/cg/controller/AppointmentController.java
@@ -5,6 +5,7 @@ import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -145,5 +146,11 @@ public class AppointmentController {
 
 		Appointment updated = service.assignPrepNurse(appointmentId, request.getEmployeeId());
 		return ResponseEntity.ok(convertToDTO(updated));
+	}
+	
+	@DeleteMapping("/appointments/{id}")
+	public String cancel(@PathVariable Integer id) {
+	    service.cancelAppointment(id);
+	    return "Appointment soft deleted";
 	}
 }
