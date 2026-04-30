4e85650180f34a5cafce7955f8439a7994127a67
Zaid <kzaid2144@gmail.com>
2026-04-30 21:42:10 +0530

    fixed import MockMvc

--- backend/HospitalManagement/src/test/java/com/cg/WebTest/StayControllerTest.java
+++ backend/HospitalManagement/src/test/java/com/cg/WebTest/StayControllerTest.java
@@ -28,6 +28,7 @@ import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc;
 import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
 
